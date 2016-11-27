45b3f54573b4dacef44bca265b42e891a78298c5
Viven <viventhraa@11street.my>
2016-11-27 21:22:54 +0800

    updating sample to show a smoother transition

--- app/src/main/java/viven/com/imagezoom/MainActivity.java
+++ app/src/main/java/viven/com/imagezoom/MainActivity.java
@@ -3,6 +3,7 @@ package viven.com.imagezoom;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 
 import viven.com.imagezoom.library.ImageZoomHelper;
 
@@ -12,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
