2a5634c79a677897dfd943107177087d7bf24926
Viven <viventhraa@11street.my>
2016-11-27 21:24:07 +0800

    adding comment

--- app/src/main/java/viven/com/imagezoom/MainActivity.java
+++ app/src/main/java/viven/com/imagezoom/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         imageZoomHelper = new ImageZoomHelper(this);
 
+        // set zoomable tag on views that is to be zoomed
         findViewById(R.id.imgLogo).setTag(R.id.zoomable, new Object());
     }
 
