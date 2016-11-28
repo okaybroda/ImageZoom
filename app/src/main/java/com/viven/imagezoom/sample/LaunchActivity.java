7906b575c1badd73407653b0018719de345cb9c9
Viven <viventhraa@11street.my>
2016-11-28 16:21:38 +0800

    updating sample

--- app/src/main/java/com/viven/imagezoom/sample/LaunchActivity.java
+++ app/src/main/java/com/viven/imagezoom/sample/LaunchActivity.java
@@ -1,7 +1,9 @@
 package com.viven.imagezoom.sample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        findViewById(R.id.btnBasic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+            }
+        });
+
+        findViewById(R.id.btnRecycler).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LaunchActivity.this, RecyclerViewActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/viven/imagezoom/sample/RecyclerViewActivity.java
+++ app/src/main/java/com/viven/imagezoom/sample/RecyclerViewActivity.java
@@ -1,13 +1,73 @@
 package com.viven.imagezoom.sample;
 
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.viven.imagezoom.ImageZoomHelper;
 
 public class RecyclerViewActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    ImageZoomHelper imageZoomHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2, RecyclerView.VERTICAL,
+                false));
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                ImageView imageView = new ImageView(RecyclerViewActivity.this);
+                imageView.setImageResource(R.mipmap.ic_launcher);
+                ImageZoomHelper.setViewZoomable(imageView);
+                imageView.setMinimumHeight(400);
+                return new RecyclerView.ViewHolder(imageView) {};
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+        });
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                ImageZoomHelper.setZoom(swipeRefreshLayout, false);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                        ImageZoomHelper.setZoom(swipeRefreshLayout, true);
+                    }
+                }, 2000);
+            }
+        });
+
+        imageZoomHelper = new ImageZoomHelper(this);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return imageZoomHelper.onDispatchTouchEvent(ev) || super.dispatchTouchEvent(ev);
     }
 }
